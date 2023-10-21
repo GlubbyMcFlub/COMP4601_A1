a89a199fba907df704fed3f0e56e9a704a471a7e
RentedZone <73078380+RentedZone@users.noreply.github.com>
2023-10-21 18:35:22 -0400

    [DAVID] Made pretty

--- server/controllers/links.js
+++ server/controllers/links.js
@@ -1,6 +1,6 @@
+import { Matrix } from "ml-matrix";
 import mongoose from "mongoose";
 import LinkModel from "../models/linkModel.js";
-import { Matrix } from 'ml-matrix';
 
 export const getLinks = async (req, res) => {
 	try {
@@ -81,9 +81,9 @@ export const searchLinks = async (req, res) => {
 	// const { query } = req.query;
 
 	try {
-		var links = await LinkModel.find().sort( {title: 1} );	//ascending by title
+		var links = await LinkModel.find().sort({ title: 1 }); //ascending by title
 		var pageRanks = new Matrix(1, links.length);
-		pageRanks.set(0,0,1);
+		pageRanks.set(0, 0, 1);
 		const alpha = 0.1;
 		const euclideanStoppingThreshold = 0.0001;
 		var probabilityMatrix = new Matrix(links.length, links.length);
@@ -98,34 +98,45 @@ export const searchLinks = async (req, res) => {
 		});
 		//if a row has all 0's, then set each entry to links.length to simulate teleportation
 		//otherwise, set each 1 to 1/numOnes to show the probability that a link will be chosen
-		for(let i = 0; i < probabilityMatrix.rows; i++){
-			const numOnes = probabilityMatrix.getRow(i).reduce((count, value) => (value === 1 ? count + 1 : count), 0);
-			if(numOnes == 0){
-				probabilityMatrix.setRow(i, Array(probabilityMatrix.columns).fill(1/links.length));
-			}
-			else{
-				let editedRow = probabilityMatrix.getRow(i).map((value) => value * 1/numOnes);
+		for (let i = 0; i < probabilityMatrix.rows; i++) {
+			const numOnes = probabilityMatrix
+				.getRow(i)
+				.reduce((count, value) => (value === 1 ? count + 1 : count), 0);
+			if (numOnes == 0) {
+				probabilityMatrix.setRow(
+					i,
+					Array(probabilityMatrix.columns).fill(1 / links.length)
+				);
+			} else {
+				let editedRow = probabilityMatrix
+					.getRow(i)
+					.map((value) => (value * 1) / numOnes);
 				probabilityMatrix.setRow(i, editedRow);
 			}
 		}
 
-
 		//multiply matrix by the chance that a link will be chosen
-		probabilityMatrix.mul(1-alpha);
+		probabilityMatrix.mul(1 - alpha);
 
 		//add the chance that a teleportation will occur
 		let teleportationMatrix = new Matrix(links.length, links.length);
-		for(let i = 0; i < probabilityMatrix.rows; i++){
-			teleportationMatrix.setRow(i, Array(probabilityMatrix.columns).fill(alpha/links.length));
+		for (let i = 0; i < probabilityMatrix.rows; i++) {
+			teleportationMatrix.setRow(
+				i,
+				Array(probabilityMatrix.columns).fill(alpha / links.length)
+			);
 		}
 		probabilityMatrix.add(teleportationMatrix);
 
 		//power iteration. multiply pageRanks matrix by probabilityMatrix until euclidean distance between last two vectors < 0.0001
 		let oldpageRanks;
-		do{
+		do {
 			oldpageRanks = pageRanks.clone();
 			pageRanks = pageRanks.mmul(probabilityMatrix);
-		}while(Math.abs(oldpageRanks.norm() - pageRanks.norm()) >= euclideanStoppingThreshold);
+		} while (
+			Math.abs(oldpageRanks.norm() - pageRanks.norm()) >=
+			euclideanStoppingThreshold
+		);
 
 		//create object which includes pageRanks
 		var rankedLinks = links.map(function (link, column = 0) {
@@ -133,13 +144,12 @@ export const searchLinks = async (req, res) => {
 				url: link.link,
 				pageRank: pageRanks.get(0, column),
 			};
-			row++;
 		});
 		rankedLinks.sort((a, b) => b.pageRank - a.pageRank);
 
 		console.log(rankedLinks);
 
-		// var pageRanks = 
+		// var pageRanks =
 		// const searchResults = index
 		// 	.search(query, {
 		// 		// TODO more experimentation and fine-tuning of our search algorithm
@@ -165,7 +175,7 @@ export const searchLinks = async (req, res) => {
 		// 		};
 		// 	});
 		// }
-		
+
 		res.status(200).json(rankedLinks);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
