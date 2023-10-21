3115d486d3676ddbc4a31f5475e00eac532d4706
Eric <2ericleroux@gmail.com>
2023-10-20 21:19:36 -0400

    did lab 5

--- server/controllers/links.js
+++ server/controllers/links.js
@@ -1,14 +1,6 @@
-import elasticlunr from "elasticlunr";
 import mongoose from "mongoose";
 import LinkModel from "../models/linkModel.js";
-
-const index = elasticlunr(function () {
-	this.addField("paragraph");
-	this.addField("title");
-	this.addField("link");
-	this.addField("outgoingLinks");
-	this.setRef("id");
-});
+import { Matrix } from 'ml-matrix';
 
 export const getLinks = async (req, res) => {
 	try {
@@ -67,15 +59,7 @@ export const createLink = async (req, res) => {
 				},
 				{ new: true }
 			);
-			await newLink.save().then((savedLink) => {
-				let doc = {
-					id: savedLink.id,
-					paragraph: savedLink.paragraph,
-					link: savedLink.link,
-					title: savedLink.title,
-				};
-				index.addDoc(doc);
-			});
+			await newLink.save();
 			res.status(200).json(newLink);
 		} else {
 			let newLink = new LinkModel({
@@ -85,20 +69,7 @@ export const createLink = async (req, res) => {
 				outgoingLinks: outgoingLinks,
 				incomingLinks: incomingLink ? [incomingLink] : [],
 			});
-			await newLink.save().then((savedLink) => {
-				if (paragraph && title) {
-					// TODO update schema with a field that indicates whether the page has been visited
-					// Create locally stored version of link after saving to the DB, only if the page has been visited
-
-					let doc = {
-						id: savedLink.id,
-						paragraph: paragraph,
-						link: link,
-						title: title,
-					};
-					index.addDoc(doc);
-				} else console.log("Page has not been visited yet");
-			});
+			await newLink.save();
 			res.status(201).json(newLink);
 		}
 	} catch (error) {
@@ -107,35 +78,95 @@ export const createLink = async (req, res) => {
 };
 
 export const searchLinks = async (req, res) => {
-	const { query } = req.query;
+	// const { query } = req.query;
 
 	try {
-		const searchResults = index
-			.search(query, {
-				// TODO more experimentation and fine-tuning of our search algorithm
-				fields: {
-					paragraph: { boost: 3 },
-					title: { boost: 2 },
-					outgoingLinks: { boost: 1 },
-				},
-			})
-			.slice(0, 10);
-		if (searchResults.length === 0) {
-			res
-				.status(404)
-				.json({ message: "No results found with query: " + query });
-		} else {
-			var links = searchResults.map(function (result) {
-				var link = index.documentStore.getDoc(result.ref);
-				return {
-					paragraph: link.paragraph,
-					title: link.title,
-					url: link.link,
-					score: result.score,
-				};
+		var links = await LinkModel.find().sort( {title: 1} );	//ascending by title
+		var pageRanks = new Matrix(1, links.length);
+		pageRanks.set(0,0,1);
+		const alpha = 0.1;
+		const euclideanStoppingThreshold = 0.0001;
+		var probabilityMatrix = new Matrix(links.length, links.length);
+
+		//populate probability matrix
+		links.forEach((link, row = 0) => {
+			link.outgoingLinks.forEach((outgoingLink) => {
+				const column = links.findIndex((link) => link.link === outgoingLink);
+				probabilityMatrix.set(row, column, 1);
 			});
+			row++;
+		});
+		//if a row has all 0's, then set each entry to links.length to simulate teleportation
+		//otherwise, set each 1 to 1/numOnes to show the probability that a link will be chosen
+		for(let i = 0; i < probabilityMatrix.rows; i++){
+			const numOnes = probabilityMatrix.getRow(i).reduce((count, value) => (value === 1 ? count + 1 : count), 0);
+			if(numOnes == 0){
+				probabilityMatrix.setRow(i, Array(probabilityMatrix.columns).fill(1/links.length));
+			}
+			else{
+				let editedRow = probabilityMatrix.getRow(i).map((value) => value * 1/numOnes);
+				probabilityMatrix.setRow(i, editedRow);
+			}
 		}
-		res.status(200).json(links);
+
+
+		//multiply matrix by the chance that a link will be chosen
+		probabilityMatrix.mul(1-alpha);
+
+		//add the chance that a teleportation will occur
+		let teleportationMatrix = new Matrix(links.length, links.length);
+		for(let i = 0; i < probabilityMatrix.rows; i++){
+			teleportationMatrix.setRow(i, Array(probabilityMatrix.columns).fill(alpha/links.length));
+		}
+		probabilityMatrix.add(teleportationMatrix);
+
+		//power iteration. multiply pageRanks matrix by probabilityMatrix until euclidean distance between last two vectors < 0.0001
+		let oldpageRanks;
+		do{
+			oldpageRanks = pageRanks.clone();
+			pageRanks = pageRanks.mmul(probabilityMatrix);
+		}while(Math.abs(oldpageRanks.norm() - pageRanks.norm()) >= euclideanStoppingThreshold);
+
+		//create object which includes pageRanks
+		var rankedLinks = links.map(function (link, column = 0) {
+			return {
+				url: link.link,
+				pageRank: pageRanks.get(0, column),
+			};
+			row++;
+		});
+		rankedLinks.sort((a, b) => b.pageRank - a.pageRank);
+
+		console.log(rankedLinks);
+
+		// var pageRanks = 
+		// const searchResults = index
+		// 	.search(query, {
+		// 		// TODO more experimentation and fine-tuning of our search algorithm
+		// 		fields: {
+		// 			paragraph: { boost: 3 },
+		// 			title: { boost: 2 },
+		// 			outgoingLinks: { boost: 1 },
+		// 		},
+		// 	})
+		// 	.slice(0, 10);
+		// if (searchResults.length === 0) {
+		// 	res
+		// 		.status(404)
+		// 		.json({ message: "No results found with query: " + query });
+		// } else {
+		// 	var links = searchResults.map(function (result) {
+		// 		var link = index.documentStore.getDoc(result.ref);
+		// 		return {
+		// 			paragraph: link.paragraph,
+		// 			title: link.title,
+		// 			url: link.link,
+		// 			score: result.score,
+		// 		};
+		// 	});
+		// }
+		
+		res.status(200).json(rankedLinks);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
