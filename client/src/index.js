aca28da1914e86ee910c3c3d61870920e7d1ef62
RentedZone <73078380+RentedZone@users.noreply.github.com>
2023-10-27 13:13:21 -0400

    [DAVID] Fixed many warnings and errors for pckgs

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import ContainerPage from "./pages/ContainerPage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "../assets/SearchPage.css";
 import Result from "../components/Result";
