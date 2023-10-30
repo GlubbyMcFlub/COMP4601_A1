a8bc04a8ddaa904b47857bb046784f6d8b3df316
RentedZone <73078380+RentedZone@users.noreply.github.com>
2023-10-30 12:09:39 -0400

    [DAVID] Moved IPs to config.env, hopefully done server file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import fetch from "node-fetch";
 import mongoose from "mongoose";
+import fetch from "node-fetch";
 
 // Routes
 import links from "./routes/links.js";
@@ -15,44 +15,45 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
+const FLOATING_IP = process.env.FLOATING_IP;
+const DISTRIBUTED_SYSTEM = process.env.DISTRIBUTED_SYSTEM;
 
 // Database Connection & Start Server
-mongoose
-	.connect(CONNECTION_URL, {
+try {
+	await mongoose.connect(CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("Connected to MongoDB. Great success!");
-		app.listen(PORT, async () => {
-			console.log(`Server is running on http://localhost:${PORT}`);
-		})
-		.then(
-			response = await fetch( "http://134.117.130.17:3000/searchengines", {
+	});
+	console.log("Connected to MongoDB. Great success!");
+
+	app.listen(PORT, async () => {
+		console.log(`Server is running on http://localhost:${PORT}`);
+
+		try {
+			const response = await fetch(DISTRIBUTED_SYSTEM, {
 				method: "PUT",
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({request_url: "http://134.117.128.87:3000"}),
+				body: JSON.stringify({ request_url: FLOATING_IP }),
 			});
-			// const outgoingLinkData = await outgoingLinkResponse.json();
-			// if (outgoingLinkResponse.status === 200) {
-			// 	c.queue(outgoingLink);
-			// } else if (outgoingLinkResponse.status != 201) {
-			// 	console.error(
-			// 		"Failed to add outgoing link. Error: ",
-			// 		outgoingLinkData.message
-			// 	);
-			// }
-		).catch((error) => {
+
+			if (response.status === 201) {
+				console.log("Successfully connected to distributed system.");
+			} else {
+				console.error(
+					"Failed to connect to distributed system, bad response recieved: ",
+					response.status
+				);
+			}
+		} catch (error) {
 			console.error("Error connecting to distributed system: ", error.message);
-			process.exit(1);
 		}
-	})
-	.catch((error) => {
-		console.error("Error connecting to MongoDB. Womp womp:", error.message);
-		process.exit(1);
 	});
+} catch (error) {
+	console.error("Error connecting to MongoDB. Womp womp:", error.message);
+	process.exit(1);
+}
 
 // Middleware
 app.use(express.json({ limit: "30mb" }));
